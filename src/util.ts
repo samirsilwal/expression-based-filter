5d1d49b5511d622c416c2bf9efa46798c62c635c
samirsilwal <samirsilwal2424@gmail.com>
2023-10-08 08:01:17 +0545

    Add util to flatten the deep object

---
+++ src/util.ts
@@ -0,0 +1,13 @@
+
+export function flatten(obj: any, roots: string[] = [], sep = '.'): Object {
+  return Object
+    .keys(obj)
+    .reduce((memo, prop: string) => {
+      return Object.assign({}, memo, Object.prototype.toString.call(obj[prop]) === '[object Object]'
+        ? flatten(obj[prop], roots.concat([prop]), sep)
+        : {
+          [roots.concat([prop]).join(sep)]: obj[prop]
+        }
+      )
+    }, {})
+}
